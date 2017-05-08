7cdc6da3dd0c4ca835ceb201b595ca53c877fb22
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-05-08 15:00:06 -0500

    I just finished lecture 134 section 11 proceeding to lecture 135 now we have a button just in case you get lost in maps we can go back to our primary position by pressing the compass

--- Pokemon jr./ViewController.swift
+++ Pokemon jr./ViewController.swift
@@ -47,15 +47,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
        
         if updateCount < 3 {
         
-        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
     mapView.setRegion(region, animated: false)
     updateCount += 1
+        } else {
+        manager.stopUpdatingLocation()
+            
+        
         }
     }
     
     
 
-    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+        let region = MKCoordinateRegionMakeWithDistance(coord, 400, 400)
+        mapView.setRegion(region, animated: true)
+
+    }
+    }
     
     
     
