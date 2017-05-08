b1f2e8de9f59c2e9eedef15c421a598a5d04ba68
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-05-08 12:43:47 -0500

    just finished lecture 131 section 11 proceeding to lecture 132 now we have a map still need to upload the location of the user :( but I will fix that

--- Pokemon jr./ViewController.swift
+++ Pokemon jr./ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    mapView.showsUserLocation = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
+    
+    
+    
 
 }
 
