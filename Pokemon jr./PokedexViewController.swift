0a4e56dcd60663d4e52cede0dfcccc8ce09fdc9c
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-05-12 11:09:20 -0500

    I just finished lecture 140 section 11 proceeding to lecture 141 now we have Pokemon in the tableview and pokemon icons yes!

--- Pokemon jr./PokedexViewController.swift
+++ Pokemon jr./PokedexViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
+class PokedexViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
@@ -25,10 +25,59 @@ class PokedexViewController: UIViewController {
       uncaughtPokemons = getAllUncaughtPokemons()
         
         
-        
-        // Do any additional setup after loading the view.
+    tableView.delegate = self
+    tableView.dataSource = self
+       
     }
 
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+          return "CAUGHT"
+            
+        } else {
+         return "UNCAUGHT"
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+        return caughtPokemons.count
+        } else {
+            return uncaughtPokemons.count
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let pokemon : Pokemon
+        if indexPath.section == 0 {
+             pokemon = caughtPokemons[indexPath.row]
+            
+        } else {
+          pokemon = uncaughtPokemons[indexPath.row]
+        }
+        
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.name
+        cell.imageView?.image = UIImage(named: pokemon.imageName!)
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
     
     
     @IBAction func mapTapped(_ sender: Any) {
