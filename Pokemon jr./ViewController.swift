0017b0db1d2b24671797abc8bc1317301861e357
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-05-17 12:25:59 -0500

    I just finished lecture 143 section 11 proceeding to lecture 144 now we can tell if we can catch pokemon or not! :) onward!!

--- Pokemon jr./ViewController.swift
+++ Pokemon jr./ViewController.swift
@@ -116,6 +116,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDeleg
     }
     
     
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation!, animated: true)
+        
+        if view.annotation! is MKUserLocation {
+        return
+        }
+        let region = MKCoordinateRegionMakeWithDistance(view.annotation!.coordinate, 200, 200)
+        mapView.setRegion(region, animated: true)
+        
+        if let coord = manager.location?.coordinate {
+            if MKMapRectContainsPoint(mapView.visibleMapRect, MKMapPointForCoordinate(coord)) {
+                print("Can catch the pokemon")
+            } else {
+            print("Pokemon is to far away ")
+            }
+        
+        
+        }
+    }
+    
+    
+    
 
     @IBAction func centerTapped(_ sender: Any) {
         if let coord = manager.location?.coordinate {
